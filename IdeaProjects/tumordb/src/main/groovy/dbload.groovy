36fb269cfe0866b991e908cf1e6d1b2b36aaf4b1
Lauren Wolfe <lulumialu@gmail.com>
2015-07-30 13:39:04 -0700

    ID error fix check

--- IdeaProjects/tumordb/src/main/groovy/dbload.groovy
+++ IdeaProjects/tumordb/src/main/groovy/dbload.groovy
@@ -1,5 +1,6 @@
 //For testing, output count
-id = 0
+def id1 = 1
+def id2 = 40000
 
 //Filename will need to be looped here from another file containing filenames and perhaps tumor
 //type (or could just rtrim the tumor type from filenames.)
@@ -13,107 +14,48 @@ def boolean read(FaunusVertex v, String file_iter) {
         version = '14jan15'
 
         //Pull in line from the tsv
-        def (object1,
-                object2,
-                correlation1,
-                sample_size1,
-                min_log_p_uncorrected1,
-                bonferroni1,
-                excluded_sample_count_a1,
-                min_log_p_unused_a1,
-                excluded_sample_count_b1,
-                min_log_p_unused_b1,
-                genomic_distance1) = file_iter.split('\t')
+        def (   String object1,
+                String object2,
+                float correlation1,
+                float sample_size1,
+                float min_log_p_uncorrected1,
+                float bonferroni1,
+                float excluded_sample_count_a1,
+                float min_log_p_unused_a1,
+                float excluded_sample_count_b1,
+                float min_log_p_unused_b1,
+                float genomic_distance1) = file_iter.split('\t')
 
         //Split bioentity column into component data
-        def (dataType1,
-                featureType1,
-                name1,
-                chr1,
-                start1,
-                end1,
-                strand1,
-                annotation1) = object1.split(':')
+        def (   String dataType1,
+                String featureType1,
+                String name1,
+                String chr1,
+                int start1,
+                int end1,
+                char strand1,
+                String annotation1) = object1.split(':')
 
         //Split bioentity column into component data
-        def (dataType2,
-                featureType2,
-                name2,
-                chr2,
-                start2,
-                end2,
-                strand2,
-                annotation2) = object2.split(':')
+        def (   String dataType2,
+                String featureType2,
+                String name2,
+                String chr2,
+                int start2,
+                int end2,
+                char strand2,
+                String annotation2) = object2.split(':')
 
-        def objectID1
-        def objectID2
+        def String objectID1
+        def String objectID2
 
         //This is for filtering by annotation type, currently both bioentities need to be code_potential_somatic for the
         //code block to execute.
         if (annotation1 == "code_potential_somatic" && annotation2 == "code_potential_somatic") {
 
             //Generate objectIDs by concatenating the tumor type, feature type and gene name
-            switch (featureType1) {
-                case "GEXB":
-                    id1 = tumor_type + 'gene' + name1
-                    objectID1 = tumor_type + ':Gene:' + name1
-                    break
-                case "GNAB":
-                    id1 = tumor_type + 'gene' + name1
-                    objectID1 = tumor_type + ':Gene:' + name1
-                    break
-                case "CNVR":
-                    id1 = tumor_type + 'gene' + name1
-                    objectID1 = tumor_type + ':Gene:' + name1
-                    break
-                case "RPPA":
-                    id1 = tumor_type + 'protein' + name1
-                    objectID1 = tumor_type + ':Protein:' + name1
-                    break
-                case "METH":
-                    id1 = tumor_type + 'methylation' + name1
-                    objectID1 = tumor_type + ':Methylation:' + name1
-                    break
-                case "MIRN":
-                    id1 = tumor_type + 'mirna' + name1
-                    objectID1 = tumor_type + ':miRNA:' + name1
-                    break
-                default:
-                    id1 = tumor_type + featureType1 + name1
-                    objectID1 = tumor_type + ':' + featureType1 + ':' + name1
-                    break
-            }
-
-            switch (featureType2) {
-                case "GEXB":
-                    id2 = tumor_type + 'gene' + name2
-                    objectID2 = tumor_type + ':Gene:' + name2
-                    break
-                case "GNAB":
-                    id2 = tumor_type + 'gene' + name2
-                    objectID2 = tumor_type + ':Gene:' + name2
-                    break
-                case "CNVR":
-                    id2 = tumor_type + 'gene' + name2
-                    objectID2 = tumor_type + ':Gene:' + name2
-                    break
-                case "RPPA":
-                    id2 = tumor_type + 'protein' + name2
-                    objectID2 = tumor_type + ':Protein:' + name2
-                    break
-                case "METH":
-                    id2 = tumor_type + 'methylation' + name2
-                    objectID2 = tumor_type + ':Methylation:' + name2
-                    break
-                case "MIRN":
-                    id2 = tumor_type + 'mirna' + name2
-                    objectID2 = tumor_type + ':miRNA:' + name2
-                    break
-                default:
-                    id2 = tumor_type + featureType2 + name2
-                    objectID2 = tumor_type + ':' + featureType1 + ':' + name2
-                    break
-            }
+            objectID1 = setObjectID(tumor_type, featureType1, name1)
+            objectID2 = setObjectID(tumor_type, featureType2, name2)
 
 /*
         def id1 = Long.parseLong(id1, 36)
@@ -121,8 +63,6 @@ def boolean read(FaunusVertex v, String file_iter) {
         def id2 = Long.parseLong(id2, 36)
         id2 = id2.longValue()
 */
-            def id1 = id
-            def id2 = ++id
 
             //Does the vertex already exist? If not, create it in the db
             v.setId(id1)
@@ -160,8 +100,39 @@ def boolean read(FaunusVertex v, String file_iter) {
             edge.setProperty("genomic_distance", genomic_distance1)
             edge.setProperty("feature_types", featureType1 + ':' + featureType2)
 
-            id++
+            id1++
+            id2++
         }
     //})
     return true
+}
+
+
+//Sets Object ID for each vertex
+def String setObjectID(String tumor_type, String featureType, String name) {
+    switch (featureType) {
+        case "GEXB":
+            objectID = tumor_type + ':Gene:' + name
+            break
+        case "GNAB":
+            objectID = tumor_type + ':Gene:' + name
+            break
+        case "CNVR":
+            objectID = tumor_type + ':Gene:' + name
+            break
+        case "RPPA":
+            objectID = tumor_type + ':Protein:' + name
+            break
+        case "METH":
+            objectID = tumor_type + ':Methylation:' + name
+            break
+        case "MIRN":
+            objectID = tumor_type + ':miRNA:' + name
+            break
+        default:
+            objectID = tumor_type + ':' + featureType + ':' + name
+            break
+    }
+
+    return objectID
 }
