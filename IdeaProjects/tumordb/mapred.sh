6e66c6521aa87601e2105b93eb975391787843bb
laurenwolfe <lulumialu@gmail.com>
2015-09-21 14:23:46 -0700

    reconfigure mapreduce

--- IdeaProjects/tumordb/mapred.sh
+++ IdeaProjects/tumordb/mapred.sh
@@ -5,6 +5,7 @@ cd /local
 rm -rf dist-tumordb
 rm -rf appdata
 mkdir -p appdata/es
+mkdir -p appdata/tmp
 mkdir -p appdata/hadoop
 mkdir -p appdata/cassandra/data
 mkdir -p appdata/cassandra/commitlog
@@ -12,4 +13,4 @@ mkdir -p appdata/cassandra/saved_caches
 git clone https://github.com/laurenwolfe/dist-tumordb.git
 cd dist-tumordb/IdeaProjects/tumordb
 docker build -t lulumialu/tumordist .
-docker run -i -t -p 8182:8182 -v /local/appdata:/opt/titan-0.5.4-hadoop2 -v /local/appdata:/opt/titan-0.5.4-hadoop2 --name graph lulumialu/tumordist
+docker run -i -t -p 8182:8182 -v /local/appdata:/opt/titan-0.5.4-hadoop2/appdata -v /local/pwfiles:/opt/titan-0.5.4-hadoop2/pwfiles --name graph lulumialu/tumordist
